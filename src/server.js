da28ebe4603b825f644e0dfd8a168c93cf3c0cb5
marinouj <marinoujoanna@gmail.com>
2022-06-15 10:12:41 +0100

    small bug

--- src/server.js
+++ src/server.js
@@ -6,35 +6,35 @@ app.use(express.json());
 var crypto = require("crypto");
 
 app.post("/webhook", (request, response) => {
-  const secret = "eeaee1820d...";
-  const payload = JSON.stringify(request.body);
-  const API_SECRET = "3b99ca1fb8.....";
-  console.log(request.body.results);
-  const devID = request.body.results.devID;
-  const signature = JSON.parse(request.headers["kaedim-signature"]);
-  /* {
-      t : 1235678,
-      v1:80e62bdd6bddf54905d3cd6e13940626e16aedd....
-    }*/
-  const checkSignature = crypto
-    .createHmac("sha256", secret)
-    .update(`${signature.t}${payload}`)
-    .digest("hex");
+    const secret = "6deb2a5...";
+    const payload = JSON.stringify(request.body);
+    const API_SECRET = "3b99ca1fb8.....";
+    console.log(request.body.results);
+    const devID = request.body.results.devID;
+    const signature = JSON.parse(request.headers["kaedim-signature"]);
+    /* {
+        t : 1235678,
+        v1:80e62bdd6bddf54905d3cd6e13940626e16aedd....
+      }*/
+    const checkSignature = crypto
+        .createHmac("sha256", secret)
+        .update(`${signature.t}${payload}`)
+        .digest("hex");
 
-  if (
-    crypto.timingSafeEqual(
-      Buffer.from(signature.v1),
-      Buffer.from(checkSignature)
-    )
-  ) {
-    // Authenticity confirmed
-    console.log("SUCCESS");
-  } else {
-    // Reject response
-    console.log("FAIL");
-  }
-  // Please make sure to return a response (even an empty one)
-  response.send();
+    if (
+        crypto.timingSafeEqual(
+            Buffer.from(signature.v1),
+            Buffer.from(checkSignature)
+        )
+    ) {
+        // Authenticity confirmed
+        console.log("SUCCESS");
+    } else {
+        // Reject response
+        console.log("FAIL");
+    }
+    // Please make sure to return a response (even an empty one)
+    response.send();
 });
 
 app.listen(4242, () => console.log("Running on port 4242"));
